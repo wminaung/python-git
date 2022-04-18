595d29c0af0f251a0344e15a0f7a108a9c0eca63
wminaung <minwin243@gmail.com>
2022-04-18 18:13:23 +0630

    adding hello.py and module.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+from module import square
+
+for i in range(10):
+    print(f"The square of {i} is {square(i)}")
+
+
+
---
+++ module.py
@@ -0,0 +1,5 @@
+def square(x):
+    return x * x
+
+def plug(x,y):
+    return x + y
